58488fd172e22f0285c607097c7b17108931596d
Arthur Pope <spartanbard@gmail.com>
2018-04-02 21:13:50 -0500

    Finally, holy christ

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+form = """
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-raduis: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="/" method="post">
+            <label for="user-number">Rotate by:</label>
+            <input type="text" id="user-number" name="rot" value="0" />
+            <textarea name="text" rows="12" cols="80"></textarea>
+            <input type="submit" value="Submit Query" />
+        
+        </form>
+    </body>
+</html>
+"""
+
+
+@app.route("/", methods=["POST"])
+def encrypt():
+    rot = request.form['rot']
+    text = request.form['text']
+    result = rotate_string(text, int(rot))
+    return "<h1>" + result + "</h1>"
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
